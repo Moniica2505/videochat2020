1d7bf348e77438d7f46c07f350aa47cae8f579e4
Moniica2505 <55800480+Moniica2505@users.noreply.github.com>
2021-01-20 11:15:18 +0100

    Create MessageRepository.java

---
+++ src/main/java/edu/uclm/esi/videochat/springdao/MessageRepository.java
@@ -0,0 +1,17 @@
+package edu.uclm.esi.videochat.springdao;
+
+import java.util.ArrayList;
+
+import javax.json.JsonObject;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import edu.uclm.esi.videochat.model.Message;
+
+public interface MessageRepository extends CrudRepository <Message, String> {
+	
+	@Query(value = "SELECT * FROM message WHERE (recipient=:recipient AND sender=:sender) OR (recipient=:sender AND sender=:recipient) ORDER BY date", nativeQuery = true)
+    public ArrayList<Message> recuperarConversacion(@Param("recipient") String recipient, @Param("sender") String sender);
+}
